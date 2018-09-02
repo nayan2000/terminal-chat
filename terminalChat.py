de58a485465231b156146b7fefad71951d4fd19e
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2018-09-02 18:44:26 +0530

    User Interaction based functions completed

--- terminalChat.py
+++ terminalChat.py
@@ -1,9 +1,21 @@
 import getpass
 from termcolor import colored
 from dotenv import load_dotenv
-load_dotenv(dotenv_path='.env')
+
+from pusher import pusher
+import pysher
+import os
+import json
+#import all the pusher app keys from env file
+try:
+    load_dotenv(dotenv_path='.env')
+except:
+    load_dotenv(dotenv_path='sample.env')
 
 class terminalChat():
+    #initially set the variables to None
+    #on valid inputs from the user, they will be assigned values
+    #see info.txt for what do these variables actually mean
     pusher = None
     channel = None
     chatroom = None
@@ -44,3 +56,22 @@ class terminalChat():
 
     #Now once a user logs in, let him select a chatroom to join
     
+    def selectChatroom(self):
+        print(colored("Info! Available chatrooms are %s" % str(self.chatrooms), "blue"))
+        chatroom = input(colored("Please select a chatroom : ", "green"))
+        if chatroom in self.chatrooms:
+            self.chatroom = chatroom
+            self.initPusher()
+        
+        else:
+            print(colored("Sorry! No such chatroom exists", "red"))
+            self.selectChatroom()
+
+    
+    #This function will get input message from the user
+
+    def getInput(self):
+        message = input(colored("{}: ".format(self.user), "green"))
+
+if __name__ == "__main__":
+    terminalChat().main()
